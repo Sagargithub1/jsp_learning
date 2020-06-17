22f825675dc7da5f5ac30b789b0854f58681853c
sagar <sagar@sagar-desktop>
2020-06-17 18:03:02 +0530

    controler package updated

--- WebLearnings/src/web/practice/controller/Logout.java
+++ WebLearnings/src/web/practice/controller/Logout.java
@@ -1,24 +1,27 @@
-package web.practice.login;
+package web.practice.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 
 /**
- * Servlet implementation class Register
+ * Servlet implementation class Logout
  */
-@WebServlet("/register")
-public class Register extends HttpServlet {
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		PrintWriter out=response.getWriter();
-		out.println("Successfully registered");
+	
+		HttpSession session=request.getSession();
+		session.removeAttribute("username");
+		session.invalidate();
+		response.sendRedirect("login.jsp");
 	}
 
 }
